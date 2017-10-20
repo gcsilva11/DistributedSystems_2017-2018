8f5f28eecd7ba09e1be8f9f1e3b014152cd5b982
Guilherme Cardoso Gomes da Silva <gcsilva@student.dei.uc.pt>
2017-10-20 17:42:34 +0100

    Criar lista de candidatos e mudei alguns pormenores das classes

--- projectRMI/src/adminPackage/VotingAdminInterface.java
+++ projectRMI/src/adminPackage/VotingAdminInterface.java
@@ -12,4 +12,6 @@ public interface VotingAdminInterface extends Remote {
 	boolean deleteDep(Department dep) throws java.rmi.RemoteException;
 	ArrayList <candidateList> getList(int type) throws java.rmi.RemoteException;
 	boolean newElection(Election election) throws java.rmi.RemoteException;
+	User findId(String s,int type) throws java.rmi.RemoteException;
+	boolean createList(candidateList cl) throws java.rmi.RemoteException;
 }
