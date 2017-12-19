94177b693c0328d5f4148dc5532300d5915d42eb
Joao Pedro Costa Ferreiro <ferreiro@student.dei.uc.pt>
2017-12-19 22:44:39 +0000

    lista eleicoes votadas

--- projetoSD2/src/Web/Beans/ListUserVotePlacesBean.java
+++ projetoSD2/src/Web/Beans/ListUserVotePlacesBean.java
@@ -14,30 +14,22 @@ public class ListUserVotePlacesBean extends RMIBean {
 
     public ArrayList<ArrayList<String>> getPlacesVoted() throws RemoteException {
         ArrayList<Integer> elecIds = this.server.getEls();
-        ArrayList<String> aux = null;
-        System.out.println(elecIds.size());
+        ArrayList<String> aux = new ArrayList<>();
+
         for (int i = 0; i < elecIds.size(); i++) {
-            System.out.println(this.server.getFaculdadeVoted(this.idUser, elecIds.get(i)));
-
-            if(this.server.getFaculdadeVoted(this.idUser, elecIds.get(i)).equals("")){
-                aux.add(Integer.toString(elecIds.get(i)));
-                aux.add(this.server.getFaculdadeVoted(this.idUser, elecIds.get(i)));
-
-                System.out.println("oi1");
-
-                if(aux!=null)
+            if(this.server.hasVoted(this.idUser,elecIds.get(i)) && this.server.userCanVote(this.idUser,elecIds.get(i)) && !this.server.isElActive(elecIds.get(i))){
+                if(this.server.getFaculdadeVoted(this.idUser, elecIds.get(i)).equals("")) {
+                    aux.add(Integer.toString(elecIds.get(i)));
+                    aux.add(this.server.getFaculdadeVoted(this.idUser, elecIds.get(i)));
                     this.placesVoted.add(aux);
-            }
-            else{
-                aux.add(Integer.toString(elecIds.get(i)));
-                aux.add("Web");
-
-                System.out.println("oi2");
-
-                if(aux!=null)
+                } else{
+                    aux.add(Integer.toString(elecIds.get(i)));
+                    aux.add("Web");
                     this.placesVoted.add(aux);
+                }
             }
-            aux = null;
+
+            aux.clear();
         }
         return this.placesVoted;
     }
